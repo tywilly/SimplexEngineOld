2132dcca7a454ba9aa765f9cd4a842be8a96dde2
Tyler <tywilly300@gmail.com>
2018-03-27 23:36:43 -0400

    Initial commit

---
+++ src/SimplexEngine.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Tyler on 3/27/2018.
+ */
+public class SimplexEngine {
+
+    public static void main(String args[]){
+
+        System.out.println("Welcome to SimplexEngine!");
+
+    }
+
+}
